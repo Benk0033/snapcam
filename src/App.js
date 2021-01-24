6dd5cf6f4479947f0eb1b8b554f73df7bfdf714c
Benk0033 <benk0033@gmail.com>
2021-01-23 21:53:49 -0800

    added login and phone frame

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import WebcamCapture from './WebcamCapture';
 import Preview from './Preview';
@@ -9,29 +9,58 @@ import {
 } from "react-router-dom";
 import Chats from './Chats';
 import ChatView from './ChatView';
+import Login from './Login';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/appSlice';
+import { auth } from './firebase';
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(login({
+          username: authUser.displayName,
+          profilePic: authUser.photoURL,
+          id: authUser.uuid,
+        }))
+      } else {
+        dispatch(logout())
+      }
+    })
+  }, [])
   return (
     <div className="app">
       <Router>
-        <div className='app__body'>
-          {/* A <Switch> looks through its children <Route>s and
+        {!user ? (
+          <Login />
+        ) : (
+            <>
+              <img src="https://lakeridgenewsonline.com/wp-content/uploads/2020/04/snapchat.jpg" alt="" className="app__logo" />
+              <div className='app__body'>
+                <div className="app__bodyBackground">
+                  {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/chats/view">
-              <ChatView />
-            </Route>
-            <Route path="/chats">
-              <Chats />
-            </Route>
-            <Route path="/preview">
-              <Preview />
-            </Route>
-            <Route exact path="/">
-              <WebcamCapture />
-            </Route>
-          </Switch>
-        </div>
+                  <Switch>
+                    <Route path="/chats/view">
+                      <ChatView />
+                    </Route>
+                    <Route path="/chats">
+                      <Chats />
+                    </Route>
+                    <Route path="/preview">
+                      <Preview />
+                    </Route>
+                    <Route exact path="/">
+                      <WebcamCapture />
+                    </Route>
+                  </Switch>
+                </div>
+              </div>
+            </>
+          )}
       </Router>
     </div>
   );
--- src/Chats.js
+++ src/Chats.js
@@ -3,27 +3,47 @@ import React, { useEffect, useState } from 'react'
 import './Chats.css'
 import SearchIcon from '@material-ui/icons/Search';
 import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
-import { db } from './firebase';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import { auth, db } from './firebase';
 import Chat from './Chat'
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUser } from './features/appSlice';
+import { useHistory } from 'react-router-dom';
+import { resetCameraImage } from './features/cameraSlice';
 
 function Chats() {
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([]);
+    const user = useSelector(selectUser);
+    const dispatch = useDispatch();
+    const history = useHistory();
 
     useEffect(() => {
-        db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot =>
-            setPosts(
-                snapshot.docs.map((doc) => ({
-                    id: doc.id,
-                    data: doc.data(),
-                }))))
+        db.collection('posts')
+            .orderBy('timestamp', 'desc')
+            .onSnapshot(snapshot =>
+                setPosts(
+                    snapshot.docs.map((doc) => ({
+                        id: doc.id,
+                        data: doc.data(),
+                    }))))
     }, [])
+
+    const takeSnap = () => {
+        dispatch(resetCameraImage())
+        history.push('/')
+    }
 
     return (
         <div className="chats">
             <div className="chats__header">
-                <Avatar className="chats__avatar" />
+                <Avatar
+                    src={user.profilePic}
+                    onClick={() => {
+                        auth.signOut()
+                    }}
+                    className="chats__avatar" />
                 <div className="chats__search">
-                    <SearchIcon />
+                    <SearchIcon className="chats__searchIcon" />
                     <input type="text" placeholder="Friends" />
                 </div>
                 <ChatBubbleIcon className="chats__chatIcon" />
@@ -43,6 +63,11 @@ function Chats() {
                     />
                 ))}
             </div>
+
+            <RadioButtonUncheckedIcon
+                className='chats__takePicIcon'
+                onClick={takeSnap}
+                fontSize='large' />
         </div>
     )
 }
